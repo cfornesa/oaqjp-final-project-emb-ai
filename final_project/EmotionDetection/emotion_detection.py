670c616cfb76f0e2145795af4b7c1b750f872404
cfornesa <cfornesa@bu.edu>
2026-03-01 05:06:00 -0500

    Phase 1 done

--- final_project/EmotionDetection/emotion_detection.py
+++ final_project/EmotionDetection/emotion_detection.py
@@ -7,15 +7,18 @@ def emotion_detector(text_to_analyse):  # Define a function named sentiment_anal
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}  # Set the headers required for the API request
     response = requests.post(url, json = myobj, headers=header)  # Send a POST request to the API with the text and headers
     formatted_response = json.loads(response.text)
-    emotions_dict = formatted_response['emotionPredictions'][0]['emotion']
-    emotions_list = list(emotions_dict.keys())
-    anger_score = emotions_dict['anger']
-    disgust_score = emotions_dict['disgust']
-    fear_score = emotions_dict['fear']
-    joy_score = emotions_dict['joy']
-    sadness_score = emotions_dict['sadness']
-    emotions = [anger_score, disgust_score, fear_score, joy_score, sadness_score]
-    max_value = max(emotions)
-    dominant_emotion = emotions_list[emotions.index(max_value)]
-    emotions_dict['dominant_emotion'] = dominant_emotion
+    if response.status_code == 200:
+        emotions_dict = formatted_response['emotionPredictions'][0]['emotion']
+        emotions_list = list(emotions_dict.keys())
+        anger_score = emotions_dict['anger']
+        disgust_score = emotions_dict['disgust']
+        fear_score = emotions_dict['fear']
+        joy_score = emotions_dict['joy']
+        sadness_score = emotions_dict['sadness']
+        emotions = [anger_score, disgust_score, fear_score, joy_score, sadness_score]
+        max_value = max(emotions)
+        dominant_emotion = emotions_list[emotions.index(max_value)]
+        emotions_dict['dominant_emotion'] = dominant_emotion
+    elif response.status_code == 400:
+        emotions_dict = {"anger": None, "disgust": None, "fear": None, "joy": None, "sadness": None, "dominant_emotion": None}
     return emotions_dict
